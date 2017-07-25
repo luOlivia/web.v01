7b5ae4b8df608d523fffe988757330cc9c432c74
Olivia Lu <olivialu46@gmail.com>
2017-07-24 21:58:07 -0400

    added a fade-in

--- js/imageformat.js
+++ js/imageformat.js
@@ -18,7 +18,13 @@
 });*/
 $(document).ready( function() {
 
+  var $boxes = $('.portfolio-item');
+  $boxes.hide();
+
   var $container = $('.js-masonry').imagesLoaded( function() {
+
+    $boxes.fadeIn();
+    
     $container.masonry({
       columnWidth: 10,
       itemSelector: '.portfolio-item'
